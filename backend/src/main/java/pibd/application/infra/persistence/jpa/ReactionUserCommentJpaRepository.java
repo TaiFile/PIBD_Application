27a578a0c78c45603779cdd6dfccd67b05f304a5
Eduardo Malagutti <eduardosm2004@gmail.com>
2025-07-12 13:32:27 -0300

    fix: deleting unecessary repository

--- backend/src/main/java/pibd/application/infra/persistence/jpa/ReactionUserCommentJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package pibd.application.infra.persistence.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pibd.application.domain.model.ReactionUserComment;
-
-@Repository
-public interface ReactionUserCommentJpaRepository extends JpaRepository<ReactionUserComment, Long> {
-}
--- backend/src/main/java/pibd/application/infra/persistence/jpa/UserJpaRepository.java
+++ backend/src/main/java/pibd/application/infra/persistence/jpa/UserJpaRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pibd.application.domain.model.User;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Repository
